51b3ba5d7b555adb1ece87b0649507eaa72411d1
ruchybrach <brach4a@gmail.com>
2023-11-03 00:07:35 -0400

    1st commit for the trial branch

--- 1 tables/1 table.sql
+++ 1 tables/1 table.sql
@@ -1,6 +1,10 @@
 use HumanBodydb
 go
-create table dbo.body(id int not null identity primary key,
+drop table if exists body
+go
+create table dbo.body(
+	id int not null identity primary key,
+	creaturetype varchar(25) not null,
     BodyPart varchar(100) not null, 
     num int not null
     )
