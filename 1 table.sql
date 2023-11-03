cac13f6950bad292bf51059e8dda7bffcf64ee5b
ruchybrach <brach4a@gmail.com>
2023-11-02 23:21:47 -0400

    table 1

--- 1 table.sql
+++ 1 table.sql
@@ -1,3 +1,6 @@
 use HumanBodydb
 go
-create table dbo.body(id int not null identity primary key)
+create table dbo.body(id int not null identity primary key,
+    BodyPart varchar(100) not null, 
+    num int not null
+    )
