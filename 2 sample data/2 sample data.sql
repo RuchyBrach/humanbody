c6398bfa41d01ae1392f5af784a913380f8d0399
RuchyBrach <143746312+RuchyBrach@users.noreply.github.com>
2023-11-03 00:20:14 -0400

    Update 2 sample data.sql

--- 2 sample data/2 sample data.sql
+++ 2 sample data/2 sample data.sql
@@ -2,7 +2,7 @@ insert body(BodyPart, num)
 select 'eyes', 2
 union select 'ears', 2
 union select 'nose', 1
-union select 'mouth', 2
+union select 'muuth', 2
 union select 'teeth', 32
 
 --added hair... how we should store the hair part in...
