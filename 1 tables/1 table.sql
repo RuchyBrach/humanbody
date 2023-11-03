c6593016eedfafb12c3185329dd1f1a3c98b713b
ruchybrach <brach4a@gmail.com>
2023-11-03 00:03:02 -0400

    made the num column nullable

--- 1 tables/1 table.sql
+++ 1 tables/1 table.sql
@@ -1,6 +1,8 @@
 use HumanBodydb
 go
+drop table if exists body 
+go
 create table dbo.body(id int not null identity primary key,
     BodyPart varchar(100) not null, 
-    num int not null
+    num int null
     )
--- 2 sample data/2 sample data.sql
+++ 2 sample data/2 sample data.sql
@@ -1,6 +1,10 @@
-insert body(BodyPart, Num)
+insert body(BodyPart, num)
 select 'eyes', 2
 union select 'ears', 2
 union select 'nose', 1
 union select 'mouth', 2
 union select 'teeth', 32
+
+--added hair... how we should store the hair part in...
+insert body(bodypart)
+select 'hair'
